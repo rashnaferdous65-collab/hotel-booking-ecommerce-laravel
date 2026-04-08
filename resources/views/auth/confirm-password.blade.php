e2460a5961131961b77d0a775a7705c2d8cf0470
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-04-09 00:31:51 +0600

    Updated confirm-password UI design

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,27 +1,37 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-    </div>
+
+    <section class="mb-4 text-sm text-gray-600">
+        {{ __('Please verify your password to continue accessing this secure section.') }}
+    </section>
 
     <form method="POST" action="{{ route('password.confirm') }}">
         @csrf
 
-        <!-- Password -->
-        <div>
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="mt-3">
+            <x-input-label for="password" value="Password" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+            <x-text-input
+                id="password"
+                name="password"
+                type="password"
+                class="w-full mt-1 block"
+                placeholder="Enter your password"
+                required
+                autocomplete="current-password"
+            />
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+            <x-input-error
+                :messages="$errors->get('password')"
+                class="mt-2 text-red-500"
+            />
         </div>
 
-        <div class="flex justify-end mt-4">
-            <x-primary-button>
-                {{ __('Confirm') }}
+        <div class="mt-5 flex justify-end">
+            <x-primary-button class="px-4 py-2">
+                Confirm Password
             </x-primary-button>
         </div>
+        
     </form>
+
 </x-guest-layout>
