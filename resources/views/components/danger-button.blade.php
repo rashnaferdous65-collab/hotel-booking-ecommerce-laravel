fc0020c8013c93d3471b861de3894bce93346d3b
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-04-18 01:13:05 +0600

    Update dropdown component design

--- resources/views/components/danger-button.blade.php
+++ resources/views/components/danger-button.blade.php
@@ -3,15 +3,21 @@
 ])
 
 @php
-    $classes = 'inline-flex items-center px-4 py-2 bg-red-600 border border-transparent 
-                rounded-md font-semibold text-xs text-white uppercase tracking-widest 
-                hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 
-                focus:ring-red-500 focus:ring-offset-2 transition ease-in-out duration-150';
+    $baseClasses = [
+        'inline-flex items-center px-4 py-2',
+        'bg-red-600 border border-transparent',
+        'rounded-md font-semibold text-xs text-white uppercase tracking-widest',
+        'hover:bg-red-500 active:bg-red-700',
+        'focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2',
+        'transition ease-in-out duration-150'
+    ];
 @endphp
 
-<button {{ $attributes->merge([
-    'type' => $type,
-    'class' => $classes
-]) }}>
+<button 
+    type="{{ $type }}"
+    {{ $attributes->merge([
+        'class' => implode(' ', $baseClasses)
+    ]) }}
+>
     {{ $slot }}
 </button>
