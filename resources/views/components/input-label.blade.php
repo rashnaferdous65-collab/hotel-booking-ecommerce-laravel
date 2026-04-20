462e1de7de5ecb4f5ef720fc2fe52cc267f44136
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-04-21 00:47:15 +0600

    Updated input-label component design

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
-@props(['value'])
+@props(['value' => null])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $value ?? $slot }}
+<label {{ $attributes->class(['block font-medium text-sm text-gray-700']) }}>
+    {{ $value ?: $slot }}
 </label>
