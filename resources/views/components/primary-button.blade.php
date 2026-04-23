fa65d8bea9c47442dd4113aa8ec6065171d28039
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-04-24 00:42:02 +0600

    Update primary button design

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,19 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
+@props([
+    'type' => 'submit'
+])
+
+@php
+    $baseClasses = 'inline-flex items-center px-4 py-2 border border-transparent 
+                    rounded-md font-semibold text-xs text-white uppercase 
+                    tracking-widest transition ease-in-out duration-150';
+
+    $stateClasses = 'bg-gray-800 hover:bg-gray-700 focus:bg-gray-700 
+                     active:bg-gray-900 focus:outline-none focus:ring-2 
+                     focus:ring-indigo-500 focus:ring-offset-2';
+
+    $classes = $baseClasses . ' ' . $stateClasses;
+@endphp
+
+<button {{ $attributes->merge(['type' => $type, 'class' => $classes]) }}>
     {{ $slot }}
 </button>
