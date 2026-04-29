cb966c1e7f0a996c9c934be7cc94afa240289551
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-04-30 00:33:29 +0600

    Add admin index blade file

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html> 
+  @include('admin.css')
+  <body>
+    @include('admin.header')
+    <div class="d-flex align-items-stretch">
+     @include('admin.slidebar')
+   
+     @include('admin.body')
+       </div>
+
+        @include('admin.footer')
