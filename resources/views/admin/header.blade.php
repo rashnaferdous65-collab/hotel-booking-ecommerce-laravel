9fdb0562c59fdb1fe940a0da647e27bf074a3b98
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-04 00:55:46 +0600

    Updated header UI with cleaner structure

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -1,154 +1,145 @@
- <header class="header">   
-      <nav class="navbar navbar-expand-lg">
+<header class="header">
+    <nav class="navbar navbar-expand-lg">
+
+        <!-- 🔍 Search Panel -->
         <div class="search-panel">
-          <div class="search-inner d-flex align-items-center justify-content-center">
-            <div class="close-btn">Close <i class="fa fa-close"></i></div>
-            <form id="searchForm" action="#">
-              <div class="form-group">
-                <input type="search" name="search" placeholder="What are you searching for...">
-                <button type="submit" class="submit">Search</button>
-              </div>
-            </form>
-          </div>
+            <div class="search-inner d-flex justify-content-center align-items-center">
+                <span class="close-btn">Close <i class="fa fa-close"></i></span>
+
+                <form id="searchForm">
+                    <div class="form-group">
+                        <input type="search" name="search" placeholder="Search here...">
+                        <button type="submit">Search</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="container-fluid d-flex align-items-center justify-content-between">
-          <div class="navbar-header">
-            <!-- Navbar Header--><a href="index.html" class="navbar-brand">
-              <div class="brand-text brand-big visible text-uppercase"><strong class="text-primary">Dark</strong><strong>Admin</strong></div>
-              <div class="brand-text brand-sm"><strong class="text-primary">D</strong><strong>A</strong></div></a>
-            <!-- Sidebar Toggle Btn-->
-            <button class="sidebar-toggle"><i class="fa fa-long-arrow-left"></i></button>
-          </div>
-          <div class="right-menu list-inline no-margin-bottom">    
-            <div class="list-inline-item"><a href="#" class="search-open nav-link"><i class="icon-magnifying-glass-browser"></i></a></div>
-            <div class="list-inline-item dropdown"><a id="navbarDropdownMenuLink1" href="http://example.com" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link messages-toggle"><i class="icon-email"></i><span class="badge dashbg-1">5</span></a>
-              <div aria-labelledby="navbarDropdownMenuLink1" class="dropdown-menu messages"><a href="#" class="dropdown-item message d-flex align-items-center">
-                  <div class="profile"><img src="{{asset('admin_css/img/avatar-3.jpg')}}" alt="..." class="img-fluid">
-                    <div class="status online"></div>
-                  </div>
-                  <div class="content">   <strong class="d-block">Nadia Halsey</strong><span class="d-block">lorem ipsum dolor sit amit</span><small class="date d-block">9:30am</small></div></a><a href="#" class="dropdown-item message d-flex align-items-center">
-                  <div class="profile"><img src="{{asset('admin_css/img/avatar-2.jpg')}}" alt="avatar" class="img-fluid">
-                    <div class="status away"></div>
-                  </div>
-                  <div class="content">   <strong class="d-block">Peter Ramsy</strong><span class="d-block">lorem ipsum dolor sit amit</span><small class="date d-block">7:40am</small></div></a><a href="#" class="dropdown-item message d-flex align-items-center">
-                  <div class="profile"><img src="{{asset('admin_css/img/avatar-1.jpg')}}" alt="..." class="img-fluid">
-                    <div class="status busy"></div>
-                  </div>
-                  <div class="content">   <strong class="d-block">Sam Kaheil</strong><span class="d-block">lorem ipsum dolor sit amit</span><small class="date d-block">6:55am</small></div></a><a href="#" class="dropdown-item message d-flex align-items-center">
-                  <div class="profile"><img src="{{asset('admin_css/img/avatar-5.jpg')}}" alt="..." class="img-fluid">
-                    <div class="status offline"></div>
-                  </div>
-                  <div class="content">   <strong class="d-block">Sara Wood</strong><span class="d-block">lorem ipsum dolor sit amit</span><small class="date d-block">10:30pm</small></div></a><a href="#" class="dropdown-item text-center message"> <strong>See All Messages <i class="fa fa-angle-right"></i></strong></a></div>
-            </div>
-            <!-- Tasks-->
-            <div class="list-inline-item dropdown"><a id="navbarDropdownMenuLink2" href="http://example.com" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link tasks-toggle"><i class="icon-new-file"></i><span class="badge dashbg-3">9</span></a>
-              <div aria-labelledby="navbarDropdownMenuLink2" class="dropdown-menu tasks-list"><a href="#" class="dropdown-item">
-                  <div class="text d-flex justify-content-between"><strong>Task 1</strong><span>40% complete</span></div>
-                  <div class="progress">
-                    <div role="progressbar" style="width: 40%" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-1"></div>
-                  </div></a><a href="#" class="dropdown-item">
-                  <div class="text d-flex justify-content-between"><strong>Task 2</strong><span>20% complete</span></div>
-                  <div class="progress">
-                    <div role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-3"></div>
-                  </div></a><a href="#" class="dropdown-item">
-                  <div class="text d-flex justify-content-between"><strong>Task 3</strong><span>70% complete</span></div>
-                  <div class="progress">
-                    <div role="progressbar" style="width: 70%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-2"></div>
-                  </div></a><a href="#" class="dropdown-item">
-                  <div class="text d-flex justify-content-between"><strong>Task 4</strong><span>30% complete</span></div>
-                  <div class="progress">
-                    <div role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-4"></div>
-                  </div></a><a href="#" class="dropdown-item">
-                  <div class="text d-flex justify-content-between"><strong>Task 5</strong><span>65% complete</span></div>
-                  <div class="progress">
-                    <div role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" class="progress-bar dashbg-1"></div>
-                  </div></a><a href="#" class="dropdown-item text-center"> <strong>See All Tasks <i class="fa fa-angle-right"></i></strong></a>
-              </div>
-            </div>
-            <!-- Tasks end-->
-            <!-- Megamenu-->
-            <div class="list-inline-item dropdown menu-large"><a href="#" data-toggle="dropdown" class="nav-link">Mega <i class="fa fa-ellipsis-v"></i></a>
-              <div class="dropdown-menu megamenu">
-                <div class="row">
-                  <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading</strong>
-                    <ul class="list-unstyled mb-3">
-                      <li><a href="#">Lorem ipsum dolor</a></li>
-                      <li><a href="#">Sed ut perspiciatis</a></li>
-                      <li><a href="#">Voluptatum deleniti</a></li>
-                      <li><a href="#">At vero eos</a></li>
-                      <li><a href="#">Consectetur adipiscing</a></li>
-                      <li><a href="#">Duis aute irure</a></li>
-                      <li><a href="#">Necessitatibus saepe</a></li>
-                      <li><a href="#">Maiores alias</a></li>
-                    </ul>
-                  </div>
-                  <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading</strong>
-                    <ul class="list-unstyled mb-3">
-                      <li><a href="#">Lorem ipsum dolor</a></li>
-                      <li><a href="#">Sed ut perspiciatis</a></li>
-                      <li><a href="#">Voluptatum deleniti</a></li>
-                      <li><a href="#">At vero eos</a></li>
-                      <li><a href="#">Consectetur adipiscing</a></li>
-                      <li><a href="#">Duis aute irure</a></li>
-                      <li><a href="#">Necessitatibus saepe</a></li>
-                      <li><a href="#">Maiores alias</a></li>
-                    </ul>
-                  </div>
-                  <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading</strong>
-                    <ul class="list-unstyled mb-3">
-                      <li><a href="#">Lorem ipsum dolor</a></li>
-                      <li><a href="#">Sed ut perspiciatis</a></li>
-                      <li><a href="#">Voluptatum deleniti</a></li>
-                      <li><a href="#">At vero eos</a></li>
-                      <li><a href="#">Consectetur adipiscing</a></li>
-                      <li><a href="#">Duis aute irure</a></li>
-                      <li><a href="#">Necessitatibus saepe</a></li>
-                      <li><a href="#">Maiores alias</a></li>
-                    </ul>
-                  </div>
-                  <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading</strong>
-                    <ul class="list-unstyled mb-3">
-                      <li><a href="#">Lorem ipsum dolor</a></li>
-                      <li><a href="#">Sed ut perspiciatis</a></li>
-                      <li><a href="#">Voluptatum deleniti</a></li>
-                      <li><a href="#">At vero eos</a></li>
-                      <li><a href="#">Consectetur adipiscing</a></li>
-                      <li><a href="#">Duis aute irure</a></li>
-                      <li><a href="#">Necessitatibus saepe</a></li>
-                      <li><a href="#">Maiores alias</a></li>
-                    </ul>
-                  </div>
-                </div>
-                <div class="row megamenu-buttons text-center">
-                  <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-1"><i class="fa fa-clock-o"></i><strong>Demo 1</strong></a></div>
-                  <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-2"><i class="fa fa-clock-o"></i><strong>Demo 2</strong></a></div>
-                  <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-3"><i class="fa fa-clock-o"></i><strong>Demo 3</strong></a></div>
-                  <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-4"><i class="fa fa-clock-o"></i><strong>Demo 4</strong></a></div>
-                  <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link bg-danger"><i class="fa fa-clock-o"></i><strong>Demo 5</strong></a></div>
-                  <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link bg-info"><i class="fa fa-clock-o"></i><strong>Demo 6</strong></a></div>
-                </div>
-              </div>
-            </div>
-            <!-- Megamenu end     -->
-            <!-- Languages dropdown    -->
-            <div class="list-inline-item dropdown"><a id="languages" rel="nofollow" data-target="#" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link language dropdown-toggle"><img src="{{asset('admin_css/img/flags/16/GB.png')}}" alt="English"><span class="d-none d-sm-inline-block">English</span></a>
-              <div aria-labelledby="languages" class="dropdown-menu"><a rel="nofollow" href="#" class="dropdown-item"> <img src="img/flags/16/DE.png" alt="English" class="mr-2"><span>German</span></a><a rel="nofollow" href="#" class="dropdown-item"> <img src="img/flags/16/FR.png" alt="English" class="mr-2"><span>French  </span></a></div>
-            </div>
-            <!-- Log out               -->
-            <div class="list-inline-item logout"> 
-           <a href="#"
-   class="nav-link"
-   onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-   Logout <i class="icon-logout"></i>
-</a>
 
-<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display:none;">
-    @csrf
-</form>
+        <div class="container-fluid d-flex justify-content-between align-items-center">
 
+            <!-- 🔹 Left Section -->
+            <div class="navbar-header d-flex align-items-center">
+                <a href="#" class="navbar-brand text-uppercase">
+                    <span class="brand-big">
+                        <strong class="text-primary">Dark</strong>Admin
+                    </span>
+                    <span class="brand-sm">
+                        <strong class="text-primary">D</strong>A
+                    </span>
+                </a>
 
-          </div>
-          </div>
+                <button class="sidebar-toggle ms-3">
+                    <i class="fa fa-long-arrow-left"></i>
+                </button>
+            </div>
+
+            <!-- 🔹 Right Menu -->
+            <ul class="list-inline mb-0 d-flex align-items-center">
+
+                <!-- Search -->
+                <li class="list-inline-item">
+                    <a href="#" class="nav-link search-open">
+                        <i class="icon-magnifying-glass-browser"></i>
+                    </a>
+                </li>
+
+                <!-- Messages -->
+                <li class="list-inline-item dropdown">
+                    <a href="#" data-toggle="dropdown" class="nav-link">
+                        <i class="icon-email"></i>
+                        <span class="badge dashbg-1">5</span>
+                    </a>
+
+                    <div class="dropdown-menu messages">
+
+                        @foreach([
+                            ['img'=>'avatar-3.jpg','name'=>'Nadia Halsey','time'=>'9:30am','status'=>'online'],
+                            ['img'=>'avatar-2.jpg','name'=>'Peter Ramsy','time'=>'7:40am','status'=>'away'],
+                            ['img'=>'avatar-1.jpg','name'=>'Sam Kaheil','time'=>'6:55am','status'=>'busy'],
+                            ['img'=>'avatar-5.jpg','name'=>'Sara Wood','time'=>'10:30pm','status'=>'offline']
+                        ] as $msg)
+
+                        <a href="#" class="dropdown-item d-flex align-items-center">
+                            <div class="profile">
+                                <img src="{{ asset('admin_css/img/'.$msg['img']) }}" class="img-fluid">
+                                <div class="status {{ $msg['status'] }}"></div>
+                            </div>
+
+                            <div class="content">
+                                <strong>{{ $msg['name'] }}</strong>
+                                <span>Lorem ipsum dolor sit amet</span>
+                                <small>{{ $msg['time'] }}</small>
+                            </div>
+                        </a>
+
+                        @endforeach
+
+                        <a href="#" class="dropdown-item text-center">
+                            <strong>See All Messages</strong>
+                        </a>
+                    </div>
+                </li>
+
+                <!-- Tasks -->
+                <li class="list-inline-item dropdown">
+                    <a href="#" data-toggle="dropdown" class="nav-link">
+                        <i class="icon-new-file"></i>
+                        <span class="badge dashbg-3">9</span>
+                    </a>
+
+                    <div class="dropdown-menu tasks-list">
+
+                        @foreach([
+                            ['task'=>'Task 1','percent'=>40],
+                            ['task'=>'Task 2','percent'=>20],
+                            ['task'=>'Task 3','percent'=>70],
+                            ['task'=>'Task 4','percent'=>30],
+                            ['task'=>'Task 5','percent'=>65],
+                        ] as $task)
+
+                        <a href="#" class="dropdown-item">
+                            <div class="d-flex justify-content-between">
+                                <strong>{{ $task['task'] }}</strong>
+                                <span>{{ $task['percent'] }}%</span>
+                            </div>
+
+                            <div class="progress">
+                                <div class="progress-bar"
+                                     style="width: {{ $task['percent'] }}%">
+                                </div>
+                            </div>
+                        </a>
+
+                        @endforeach
+
+                    </div>
+                </li>
+
+                <!-- Language -->
+                <li class="list-inline-item dropdown">
+                    <a href="#" data-toggle="dropdown" class="nav-link">
+                        <img src="{{ asset('admin_css/img/flags/16/GB.png') }}">
+                        English
+                    </a>
+                </li>
+
+                <!-- Logout -->
+                <li class="list-inline-item">
+                    <a href="#"
+                       class="nav-link"
+                       onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        Logout <i class="icon-logout"></i>
+                    </a>
+
+                    <form id="logout-form"
+                          action="{{ route('logout') }}"
+                          method="POST"
+                          class="d-none">
+                        @csrf
+                    </form>
+                </li>
+
+            </ul>
         </div>
-      </nav>
-    </header>
+    </nav>
+</header>
