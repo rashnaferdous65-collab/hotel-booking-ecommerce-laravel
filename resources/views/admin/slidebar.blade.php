4922a7d31da967b30b17c14cf39ac0740508d269
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-08 00:21:40 +0600

    Updated sidebar navigation design

--- resources/views/admin/slidebar.blade.php
+++ resources/views/admin/slidebar.blade.php
@@ -1,27 +1,89 @@
-     <!-- Sidebar Navigation-->
-      <nav id="sidebar">
-        <!-- Sidebar Header-->
-        <div class="sidebar-header d-flex align-items-center">
-          <div class="avatar"><img src="img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
-          <div class="title">
-            <h1 class="h5">Mark Stephen</h1>
-            <p>Web Designer</p>
-          </div>
+<!-- Sidebar Navigation Start -->
+<nav id="sidebar">
+
+    <!-- Profile Section -->
+    <div class="sidebar-header d-flex align-items-center">
+        
+        <div class="avatar">
+            <img src="img/avatar-6.jpg" alt="Admin Image"
+                class="img-fluid rounded-circle">
         </div>
-        <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
-        <ul class="list-unstyled">
-                <li class="active"><a href="{{url('admin/dashboard')}}"> <i class="icon-home"></i>Home </a></li>
-                <li><a href="{{url('hotel_room')}}"> <i class="icon-grid"></i> Hotel Room Details </a></li>
-              
-                <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Rooms Creditials </a>
-                  <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                    <li><a href="{{url('view_room')}}">View Room Details</a></li>
-                 
-                  
-                  </ul>
+
+        <div class="user-info">
+            <h1 class="h5 mb-0">Mark Stephen</h1>
+            <p class="mb-0">Web Designer</p>
+        </div>
+
+    </div>
+
+    <!-- Sidebar Menu Title -->
+    <span class="heading">Dashboard Menu</span>
+
+    <!-- Sidebar Menu -->
+    <ul class="list-unstyled sidebar-menu">
+
+        <!-- Dashboard -->
+        <li class="active">
+            <a href="{{url('admin/dashboard')}}">
+                <i class="icon-home"></i>
+                <span>Dashboard</span>
+            </a>
+        </li>
+
+        <!-- Hotel Room -->
+        <li>
+            <a href="{{url('hotel_room')}}">
+                <i class="icon-grid"></i>
+                <span>Hotel Room Details</span>
+            </a>
+        </li>
+
+        <!-- Room Credentials Dropdown -->
+        <li>
+            <a href="#roomDropdown"
+                data-toggle="collapse"
+                aria-expanded="false">
+
+                <i class="icon-windows"></i>
+                <span>Rooms Credentials</span>
+            </a>
+
+            <ul id="roomDropdown" class="collapse list-unstyled">
+
+                <li>
+                    <a href="{{url('view_room')}}">
+                        View Room Details
+                    </a>
                 </li>
-          <li><a href="{{url('booking_room')}}"> <i class="icon-grid"></i> View Booking Details </a></li>
-            <li><a href="{{url('gallery')}}"> <i class="icon-user"></i> View Gallery  </a></li>
-              <li><a href="{{url('message')}}"> <i class="icon-user"></i>  Customer Message  </a></li>
-      </nav>
-      <!-- Sidebar Navigation end-->
+
+            </ul>
+        </li>
+
+        <!-- Booking Details -->
+        <li>
+            <a href="{{url('booking_room')}}">
+                <i class="icon-grid"></i>
+                <span>Booking Details</span>
+            </a>
+        </li>
+
+        <!-- Gallery -->
+        <li>
+            <a href="{{url('gallery')}}">
+                <i class="icon-picture"></i>
+                <span>Gallery</span>
+            </a>
+        </li>
+
+        <!-- Customer Message -->
+        <li>
+            <a href="{{url('message')}}">
+                <i class="icon-mail"></i>
+                <span>Customer Messages</span>
+            </a>
+        </li>
+
+    </ul>
+
+</nav>
+<!-- Sidebar Navigation End -->
