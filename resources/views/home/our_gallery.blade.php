175b6b736aa33d8a0eb84b8047d858d869bbd59d
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-12 00:04:05 +0600

    Added our gallery blade file

---
+++ resources/views/home/our_gallery.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      @include('home.css')
+   </head>
+   <body>
+      @include('home.head_inner')
+    
+      
+
+  
+     
+      @include('home.gallery')
+    
+      
+      @include('home.footer')
+   </body>
+</html>
