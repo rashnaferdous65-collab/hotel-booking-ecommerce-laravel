235e860bf596bb6d97a86d6dbdec712681d681b6
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-12 00:10:23 +0600

    Updated gallery page changes

--- resources/views/home/our_gallery.blade.php
+++ resources/views/home/our_gallery.blade.php
@@ -1,18 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      @include('home.css')
-   </head>
-   <body>
-      @include('home.head_inner')
-    
-      
 
-  
-     
-      @include('home.gallery')
+<head>
     
-      
-      @include('home.footer')
-   </body>
+    @include('home.css')
+
+</head>
+
+<body>
+
+    {{-- Header Section --}}
+    @include('home.head_inner')
+
+
+    {{-- Gallery Section --}}
+    <section class="gallery-section">
+
+        @include('home.gallery')
+
+    </section>
+
+
+    {{-- Footer Section --}}
+    @include('home.footer')
+
+</body>
+
 </html>
