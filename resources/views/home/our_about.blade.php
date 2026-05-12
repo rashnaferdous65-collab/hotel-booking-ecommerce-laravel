ad830f939f1daf50053c9f423ab381c391a70f77
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-13 00:43:32 +0600

    Added our about page

---
+++ resources/views/home/our_about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      @include('home.css')
+   </head>
+   <body>
+      @include('home.head_inner')
+    
+      @include('home.about')
+
+      @include('home.footer')
+   </body>
+</html>
