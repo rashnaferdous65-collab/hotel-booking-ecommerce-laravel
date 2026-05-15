a58965eb361904d143a88b806d255bcc877d48d6
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-16 00:10:01 +0600

    Updated gallery page design

--- resources/views/home/gallery.blade.php
+++ resources/views/home/gallery.blade.php
@@ -1,23 +1,38 @@
-<div class="gallery">
+<section class="gallery_section">
    <div class="container">
+
       <div class="row">
-         <div class="col-md-12">
-            <div class="titlepage">
-               <h2>Gallery</h2>
+         <div class="col-lg-12">
+            <div class="section_title text-center">
+               <h2>Our Gallery</h2>
+               <p>Explore some beautiful moments from our hotel</p>
             </div>
          </div>
       </div>
 
       <div class="row">
-         @foreach($gallery as $item)
-            <div class="col-md-3 col-sm-6" style="margin-bottom: 30px;">
-               <div class="gallery_img">
-                  <figure>
-                     <img src="gallery_img/{{$item->image}}" alt="#" style="width: 100%; height: 200px; object-fit: cover;"/>
-                  </figure>
+
+         @foreach($gallery as $photo)
+
+            <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+
+               <div class="gallery_card">
+
+                  <div class="gallery_image">
+                     <img 
+                        src="{{ asset('gallery_img/'.$photo->image) }}" 
+                        alt="Gallery Image"
+                        style="width:100%; height:220px; object-fit:cover; border-radius:10px;"
+                     >
+                  </div>
+
                </div>
+
             </div>
+
          @endforeach
+
       </div>
+
    </div>
-</div>
+</section>
