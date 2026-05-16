93015c9d9285022fc73794a0fe06b4c391c574c8
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-17 00:18:51 +0600

    Redesigned footer section

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -1,66 +1,122 @@
-  <!--  footer -->
-      <footer>
-         <div class="footer">
-            <div class="container">
-               <div class="row">
-                  <div class=" col-md-4">
-                     <h3>Contact US</h3>
-                     <ul class="conta">
-                        <li><i class="fa fa-map-marker" aria-hidden="true"></i> Address</li>
-                        <li><i class="fa fa-mobile" aria-hidden="true"></i> +01 1234569540</li>
-                        <li> <i class="fa fa-envelope" aria-hidden="true"></i><a href="#"> demo@gmail.com</a></li>
-                     </ul>
-                  </div>
-                  <div class="col-md-4">
-                     <h3>Menu Link</h3>
-                     <ul class="link_menu">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="about.html"> about</a></li>
-                        <li><a href="room.html">Our Room</a></li>
-                        <li><a href="gallery.html">Gallery</a></li>
-                        <li><a href="blog.html">Blog</a></li>
-                        <li><a href="contact.html">Contact Us</a></li>
-                     </ul>
-                  </div>
-                  <div class="col-md-4">
-                     <h3>News letter</h3>
-                     <form class="bottom_form">
-                        <input class="enter" placeholder="Enter your email" type="text" name="Enter your email">
-                        <button class="sub_btn">subscribe</button>
-                     </form>
-                     <ul class="social_icon">
-                        <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-                        <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
-                     </ul>
-                  </div>
-               </div>
-            </div>
-            <div class="copyright">
-               <div class="container">
-                  <div class="row">
-                     <div class="col-md-10 offset-md-1">
-                        
-                        <p>
-                        © 2019 All Rights Reserved. Design by  Rashna Ferdous</a>
-                        <br><br>
-                      Project By  Rashna Ferdous
-                        </p>
+<!-- Footer Section -->
+<footer class="footer">
+   <div class="container">
+      <div class="row">
 
-                     </div>
-                  </div>
-               </div>
+         <!-- Contact Info -->
+         <div class="col-md-4">
+            <div class="footer_box">
+               <h3>Get In Touch</h3>
+
+               <ul class="conta">
+                  <li>
+                     <i class="fa fa-map-marker" aria-hidden="true"></i>
+                     Hotel Management, Bangladesh
+                  </li>
+
+                  <li>
+                     <i class="fa fa-phone" aria-hidden="true"></i>
+                     +01 1234569540
+                  </li>
+
+                  <li>
+                     <i class="fa fa-envelope" aria-hidden="true"></i>
+                     <a href="mailto:demo@gmail.com">demo@gmail.com</a>
+                  </li>
+               </ul>
             </div>
          </div>
-      </footer>
-      <!-- end footer -->
-      <!-- Javascript files-->
-      <script src="js/jquery.min.js"></script>
-      <script src="js/bootstrap.bundle.min.js"></script>
-      <script src="js/jquery-3.0.0.min.js"></script>
-      <!-- sidebar -->
-      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-      <script src="js/custom.js"></script>
-   </body>
+
+         <!-- Quick Links -->
+         <div class="col-md-4">
+            <div class="footer_box">
+               <h3>Quick Links</h3>
+
+               <ul class="link_menu">
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/about">About</a></li>
+                  <li><a href="/our_room">Rooms</a></li>
+                  <li><a href="/gallery">Gallery</a></li>
+                  <li><a href="/blog">Blog</a></li>
+                  <li><a href="/contact">Contact</a></li>
+               </ul>
+            </div>
+         </div>
+
+         <!-- Newsletter -->
+         <div class="col-md-4">
+            <div class="footer_box">
+               <h3>Newsletter</h3>
+
+               <form class="bottom_form">
+                  <input 
+                     class="enter"
+                     type="email"
+                     name="email"
+                     placeholder="Enter Your Email"
+                  >
+
+                  <button class="sub_btn">
+                     Subscribe
+                  </button>
+               </form>
+
+               <!-- Social Icons -->
+               <ul class="social_icon">
+                  <li>
+                     <a href="#">
+                        <i class="fa fa-facebook" aria-hidden="true"></i>
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#">
+                        <i class="fa fa-instagram" aria-hidden="true"></i>
+                     </a>
+                  </li>
+
+                  <li>
+                     <a href="#">
+                        <i class="fa fa-linkedin" aria-hidden="true"></i>
+                     </a>
+                  </li>
+               </ul>
+            </div>
+         </div>
+
+      </div>
+   </div>
+
+   <!-- Copyright -->
+   <div class="copyright">
+      <div class="container">
+         <div class="row">
+            <div class="col-md-12 text-center">
+
+               <p>
+                  © 2026 All Rights Reserved |
+                  Designed & Developed By
+                  <strong>Rashna Ferdous</strong>
+               </p>
+
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
+
+<!-- Javascript Files -->
+<script src="js/jquery.min.js"></script>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/jquery-3.0.0.min.js"></script>
+<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+<script src="js/custom.js"></script>
+
+</body>
 </html>
