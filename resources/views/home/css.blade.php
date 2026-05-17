752e3ad2ced101b8419c4d54efb4ac3b07a0b8e8
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-18 00:41:21 +0600

    Modified css blade head section

--- resources/views/home/css.blade.php
+++ resources/views/home/css.blade.php
@@ -1,20 +1,30 @@
 <head>
-    <meta charset="utf-8">
+    <!-- Basic -->
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+
+    <!-- Mobile Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Website Info -->
     <title>Hotel MoonDust</title>
-    <meta name="keywords" content="">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Luxury Hotel & Resort">
+    <meta name="keywords" content="hotel, resort, booking, room">
+    <meta name="author" content="Hotel MoonDust">
 
-    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-    <link rel="icon" href="{{ asset('images/fevicon.png') }}" type="image/gif" />
-    <link rel="stylesheet" href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('images/fevicon.png') }}" type="image/png">
 
-    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+    <!-- CSS Files -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/jquery.mCustomScrollbar.min.css') }}" rel="stylesheet">
 
-    </head>
+    <!-- External CSS -->
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css"
+          rel="stylesheet"
+          media="screen">
+</head>
