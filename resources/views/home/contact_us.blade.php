d832d4d8ac9d46fb50a9aa3ce018eac6fa49d4e1
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-19 00:46:39 +0600

    Updated contact page structure

---
+++ resources/views/home/contact_us.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('home.css')
+</head>
+
+<body>
+
+    {{-- Header Section --}}
+    @include('home.head_inner')
+
+    <main>
+        {{-- Contact Section --}}
+        @include('home.contact')
+    </main>
+
+    {{-- Footer Section --}}
+    @include('home.footer')
+
+</body>
+
+</html>
