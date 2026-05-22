c4bd7db44dd15719dcd93ef7093d9d8e6c7b36cb
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-22 23:49:09 +0600

    Added blog_us blade page

---
+++ resources/views/home/blog_us.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      @include('home.css')
+   </head>
+   <body>
+      @include('home.head_inner')
+    
+      @include('home.blog')
+     
+      @include('home.footer')
+   </body>
+</html>
