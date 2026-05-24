e5a4b6f06c7ddd20cd595e931bb8fc5d73fd434d
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-05-25 00:46:11 +0600

    Updated banner section design

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -1,60 +1,89 @@
-  <!-- banner -->
-      <section class="banner_main">
-         <div id="myCarousel" class="carousel slide banner" data-ride="carousel">
-            <ol class="carousel-indicators">
-               <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-               <li data-target="#myCarousel" data-slide-to="1"></li>
-               <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-               <div class="carousel-item active">
-                  <img class="first-slide" src="images/banner1.jpg" alt="First slide">
-                  <div class="container">
-                  </div>
-               </div>
-               <div class="carousel-item">
-                  <img class="second-slide" src="images/banner2.jpg" alt="Second slide">
-               </div>
-               <div class="carousel-item">
-                  <img class="third-slide" src="images/banner3.jpg" alt="Third slide">
-               </div>
-            </div>
-            <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-            </a>
+<!-- Banner Section -->
+<section class="hero_banner">
+
+   <div id="hotelSlider" class="carousel slide" data-ride="carousel">
+
+      <!-- Indicators -->
+      <ul class="carousel-indicators">
+         <li data-target="#hotelSlider" data-slide-to="0" class="active"></li>
+         <li data-target="#hotelSlider" data-slide-to="1"></li>
+         <li data-target="#hotelSlider" data-slide-to="2"></li>
+      </ul>
+
+      <!-- Slider Images -->
+      <div class="carousel-inner">
+
+         <div class="carousel-item active">
+            <img src="images/banner1.jpg" class="d-block w-100" alt="banner image">
          </div>
-         <div class="booking_ocline">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-5">
-                     <div class="book_room">
-                        <h1>Book a Room Online</h1>
-                        <form class="book_now">
-                           <div class="row">
-                              <div class="col-md-12">
-                                 <span>Arrival</span>
-                                 <img class="date_cua" src="images/date.png">
-                                 <input class="online_book" placeholder="dd/mm/yyyy" type="date" name="dd/mm/yyyy">
-                              </div>
-                              <div class="col-md-12">
-                                 <span>Departure</span>
-                                 <img class="date_cua" src="images/date.png">
-                                 <input class="online_book" placeholder="dd/mm/yyyy" type="date" name="dd/mm/yyyy">
-                              </div>
-                              <div class="col-md-12">
-                                 <button class="book_btn">Book Now</button>
-                              </div>
-                           </div>
-                        </form>
+
+         <div class="carousel-item">
+            <img src="images/banner2.jpg" class="d-block w-100" alt="banner image">
+         </div>
+
+         <div class="carousel-item">
+            <img src="images/banner3.jpg" class="d-block w-100" alt="banner image">
+         </div>
+
+      </div>
+
+      <!-- Controls -->
+      <a class="carousel-control-prev" href="#hotelSlider" role="button" data-slide="prev">
+         <span class="carousel-control-prev-icon"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#hotelSlider" role="button" data-slide="next">
+         <span class="carousel-control-next-icon"></span>
+      </a>
+
+   </div>
+
+   <!-- Booking Area -->
+   <div class="booking_section">
+      <div class="container">
+         <div class="row justify-content-start">
+
+            <div class="col-lg-5 col-md-7">
+
+               <div class="booking_form">
+
+                  <h2>Reserve Your Room</h2>
+
+                  <form action="" method="">
+
+                     <div class="form-group">
+                        <label>Check In</label>
+
+                        <div class="date_box">
+                           <img src="images/date.png" alt="">
+                           <input type="date" class="form-control" name="check_in">
+                        </div>
                      </div>
-                  </div>
+
+                     <div class="form-group">
+                        <label>Check Out</label>
+
+                        <div class="date_box">
+                           <img src="images/date.png" alt="">
+                           <input type="date" class="form-control" name="check_out">
+                        </div>
+                     </div>
+
+                     <div class="form-group">
+                        <button type="submit" class="btn btn-dark btn-block">
+                           Check Availability
+                        </button>
+                     </div>
+
+                  </form>
+
                </div>
+
             </div>
+
          </div>
-      </section>
-      <!-- end banner -->
+      </div>
+   </div>
+
+</section>
+<!-- End Banner Section -->
