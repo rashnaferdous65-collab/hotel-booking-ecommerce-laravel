1ca479ceab6d682e84005b4f78ffe655d87a34ae
Rashna Ferdous <rashnaferdous65@gmail.com>
2026-06-02 01:38:43 +0600

    Redesign dashboard layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,22 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-bold text-2xl text-gray-800">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+    <div class="py-12 bg-gray-50 min-h-screen">
+        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white rounded-lg shadow-lg">
+                <div class="p-8">
+                    <div class="border-l-4 border-blue-500 pl-4">
+                        <h3 class="text-xl font-semibold text-gray-800">
+                            {{ __('User Dashboard') }}
+                        </h3>
+                        <p class="mt-2 text-gray-600">
+                            {{ __("You're logged in!") }}
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
